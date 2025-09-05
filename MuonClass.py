0f51737a68ed0bd2209de5cc0829a3144cbba1e3
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-09-05 15:40:37 -0700

    transferred over information needed for individual muons

--- MuonClass.py
+++ MuonClass.py
@@ -1,3 +1,13 @@
 class Muon:
-    def __init__(self):
-        pass
+    def __init__(self, muon_info : str):
+        self.m_info = muon_info
+        self.m_pt = None
+        self.m_eta = None
+        self.m_phi = None
+        self.m_mass = None
+        self.m_px = None
+        self.m_py = None
+        self.m_pz = None
+        self.m_e = None
+        self.m_fvector = None
+        self.m_imass = None
