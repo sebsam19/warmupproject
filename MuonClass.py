99816b550ad6e3e002a21c671c77b1d4b7a00c7e
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-09-05 18:30:21 -0700

    moved over setting up muons phi, eta, pt, and mass. also calculated regular momentum

--- MuonClass.py
+++ MuonClass.py
@@ -1,3 +1,5 @@
+import math
+
 class Muon:
     def __init__(self, muon_info : str):
         self.m_info = muon_info
@@ -11,9 +13,25 @@ class Muon:
         self.m_e = None
         self.m_fvector = None
         self.m_imass = None
+        self.m_mntm = None
     
-    def muons_collector(self, muon_info:str) -> list:
-        begin_index = muon_info.find("=")
-        end_index = muon_info.index("dptinv")
-        data_list = muon_info[begin_index+1:end_index].strip(" ").split(" ")
+    def muons_collector(self) -> list:
+        begin_index = self.m_info.find("=")
+        end_index = self.m_info.index("dptinv")
+        data_list = self.m_info[begin_index+1:end_index].strip(" ").split(" ")
         return data_list
+    
+    def muons_setters(self) -> None:
+        data_list = self.muons_collector()
+        self.m_pt = data_list[0]
+        self.m_eta = data_list[1]
+        self.m_phi = data_list[2]
+        self.m_mass = data_list[3]
+    
+    def momentum_calc(self) -> None:
+        self.m_px = self.m_pt * math.cos(self.m_phi)
+        self.m_py = self.m_pt * math.sin(self.m_phi)
+        self.m_pz = self.m_pt * math.sinh(self.m_eta)
+
+        self.m_mntm = math.sqrt((self.m_px ** 2) + (self.m_py ** 2) + (self.m_pz ** 2))
+        
