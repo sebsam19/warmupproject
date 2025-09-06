00dd88cf4b1e025e3c5447628ba277ffc56553f3
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-09-06 10:28:02 -0700

    completed main

--- main.py
+++ main.py
@@ -42,11 +42,22 @@ from MuonClass import Muon
 
 def main():
     file_input = input()
-    
+    list_of_events_output = file_sorting(file_input)
+    file_write(list_of_events_output)
 
-    
+
+def file_write(list_of_events: list) -> None:
+    """writes the invariant mass and the specific event to the file 'masses.txt'"""
+    with open("masses.txt", "w", encoding="utf-8") as output_file:
+        for event_objs in list_of_events:
+            event_objs.i_mass_calc()
+            output_file.write(f"{event_objs.event_info_getter()}: invariant mass = {event_objs.i_mass_getter()}\n")
+
+
 
 def file_sorting(file_name : str) -> list:
+    """file is read and then sorts out 'run', 'm1', and 'm2' to create event objects and 
+    its correlated muons, then returns a list of the event objects to be written into a file"""
     event_objs = []
     with open(file_name, 'r', encoding="utf-8") as data_file:
         new_event = None
