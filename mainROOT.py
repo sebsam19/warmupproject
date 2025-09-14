a99d9fac9781c50910440a36d4e262830bd1cf9b
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-09-14 16:02:52 -0700

    new main that uses root cern to do calculations

---
+++ mainROOT.py
@@ -0,0 +1,29 @@
+import ROOT
+
+def main():
+    input_file = input()
+    file_read_write(input_file)
+    # file_writing(events_muons)
+
+def file_read_write(file_name : str) -> None:
+    with open(file_name, "r", encoding="utf-8") as data_file, open("masses.txt", "w", encoding="utf-8") as output_file:
+        curr_event = []
+        for lines in data_file:
+            if lines.startswith("Run"):
+                curr_event.append(lines.strip("\n"))
+            elif lines.startswith("m1") or lines.startswith("m2"):
+                begin_index = lines.find("=")
+                end_index = lines.find("dptinv")
+                extracted_data = lines[begin_index+1:end_index].split()
+                curr_event.append(extracted_data)
+                if lines.startswith("m2"):
+                    tvector_one = ROOT.TLorentzVector()
+                    tvector_two = ROOT.TLorentzVector()
+                    tvector_one.SetPtEtaPhiM(float(curr_event[1][0]), float(curr_event[1][1]), float(curr_event[1][2]), float(curr_event[1][3]))
+                    tvector_two.SetPtEtaPhiM(float(curr_event[2][0]), float(curr_event[2][1]), float(curr_event[2][2]), float(curr_event[2][3]))
+                    invariant_mass = (tvector_one + tvector_two).M()
+                    output_file.write(f"{curr_event[0]} : {invariant_mass}\n")
+                    curr_event = []
+
+if __name__ == "__main__":
+    main() 
