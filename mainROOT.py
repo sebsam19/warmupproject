f734ae9c9bce078ca13b865b699ce9be8be3fa86
Sebastian Bello-Caballero <sbelloca@uci.edu>
2025-09-15 20:10:03 -0700

    completed main using roots

--- mainROOT.py
+++ mainROOT.py
@@ -6,7 +6,9 @@ def main():
     # file_writing(events_muons)
 
 def file_read_write(file_name : str) -> None:
+    histogram = ROOT.TH1F("histo", "MyFirstHisto", 30, 380, 620)
     with open(file_name, "r", encoding="utf-8") as data_file, open("masses.txt", "w", encoding="utf-8") as output_file:
+
         curr_event = []
         for lines in data_file:
             if lines.startswith("Run"):
@@ -23,7 +25,11 @@ def file_read_write(file_name : str) -> None:
                     tvector_two.SetPtEtaPhiM(float(curr_event[2][0]), float(curr_event[2][1]), float(curr_event[2][2]), float(curr_event[2][3]))
                     invariant_mass = (tvector_one + tvector_two).M()
                     output_file.write(f"{curr_event[0]} : {invariant_mass}\n")
+                    histogram.Fill(invariant_mass)
                     curr_event = []
+    
+    with ROOT.TFile.Open("histooutput.root", "RECREATE") as histo_file:
+        histo_file.WriteObject(histogram, histogram.GetName())
 
 if __name__ == "__main__":
     main() 
